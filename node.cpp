24bb1372625d674bd83e64357dcd6cec70912f82
Yue Hao <yuehao031114@outlook.com>
2025-09-22 23:43:59 +0100

    feat: implement DoublyLinkedList with basic operations
    - add DNode struct with prev and next pointers
    
    - implement DoublyLinkedList class managing head and tail
    
    - add push_front and push_back for inserting at both ends
    
    - add erase to remove first occurrence of a value
    
    - add print_forward and print_backward for traversal
    
    - add destructor to properly free all nodes
    
    - include demo in main() testing all operations

--- node.cpp
+++ node.cpp
@@ -11,6 +11,7 @@ Node *next;
 */
 // 单链表的地址域是存储的下一个节点的地址 即每个节点只能向后遍历，不能向前遍历
 // 双向链表的地址域存储了前后各自的地址
+#if 0
 struct Node
 {
     Node(int data = 0) : data_(data), next_(nullptr) {};
@@ -45,3 +46,110 @@ public:
 private:
     Node *head_; // 指向链表的头节点
 };
+#endif
+// 双向链表节点
+struct DNode {
+    int val;
+    DNode* prev;
+    DNode* next;
+    DNode(int x) : val(x), prev(nullptr), next(nullptr) {}
+};
+
+class DoublyLinkedList {
+public:
+    DoublyLinkedList() : head(nullptr), tail(nullptr) {}
+
+    ~DoublyLinkedList() {
+        DNode* cur = head;
+        while (cur) {
+            DNode* temp = cur;
+            cur = cur->next;
+            delete temp;
+        }
+    }
+
+    // 头插
+    void push_front(int val) {
+        DNode* node = new DNode(val);
+        if (!head) { // 空链表
+            head = tail = node;
+        } else {
+            node->next = head;
+            head->prev = node;
+            head = node;
+        }
+    }
+
+    // 尾插
+    void push_back(int val) {
+        DNode* node = new DNode(val);
+        if (!tail) { // 空链表
+            head = tail = node;
+        } else {
+            tail->next = node;
+            node->prev = tail;
+            tail = node;
+        }
+    }
+
+    // 删除第一次出现的值
+    void erase(int val) {
+        DNode* cur = head;
+        while (cur) {
+            if (cur->val == val) {
+                if (cur->prev) cur->prev->next = cur->next;
+                else head = cur->next;  // 删除头节点
+
+                if (cur->next) cur->next->prev = cur->prev;
+                else tail = cur->prev;  // 删除尾节点
+
+                delete cur;
+                return;
+            }
+            cur = cur->next;
+        }
+    }
+
+    // 从头到尾打印
+    void print_forward() {
+        DNode* cur = head;
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->next;
+        }
+        cout << endl;
+    }
+
+    // 从尾到头打印
+    void print_backward() {
+        DNode* cur = tail;
+        while (cur) {
+            cout << cur->val << " ";
+            cur = cur->prev;
+        }
+        cout << endl;
+    }
+
+private:
+    DNode* head;
+    DNode* tail;
+};
+
+int main() {
+    DoublyLinkedList dll;
+
+    dll.push_back(1);
+    dll.push_back(2);
+    dll.push_back(3);
+    dll.print_forward();    // 1 2 3
+    dll.print_backward();   // 3 2 1
+
+    dll.push_front(0);
+    dll.print_forward();    // 0 1 2 3
+
+    dll.erase(2);
+    dll.print_forward();    // 0 1 3
+    dll.print_backward();   // 3 1 0
+
+    return 0;
+}
